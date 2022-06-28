25dbe12b8b2a4cce74c8fa846a840b0b1270a23d
Sourav Kumar <souravkumar4997@gmail.com>
2022-06-28 16:27:03 +0530

    Fix for CSRF

--- src/main/java/com/eazybytes/eazyschool/KumarHotelApplication.java
+++ src/main/java/com/eazybytes/eazyschool/KumarHotelApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EazyschoolApplication {
+public class KumarHotelApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EazyschoolApplication.class, args);
+		SpringApplication.run(KumarHotelApplication.class, args);
 	}
 
 }
